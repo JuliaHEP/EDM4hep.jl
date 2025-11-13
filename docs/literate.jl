a76e517fad0d56fa9b36af7b798467e45d92bbfe
Pere Mato <pere.mato@cern.ch>
2025-11-13 09:05:31 +0100

    Avoid running the notebooks in the CI

--- docs/literate.jl
+++ docs/literate.jl
@@ -9,7 +9,11 @@ examplesdir =  joinpath(@__DIR__, "src/examples")
 file = "$(name).lit"
 
 println("====> Processing $name")
-Literate.notebook(joinpath(examplesdir, file), examplesdir, name = name, execute = false, documenter = true, credit = true)
-Literate.script(joinpath(examplesdir, file), examplesdir, name = name, keep_comments = false, documenter = true, credit = false)
-Literate.markdown(joinpath(examplesdir, file), examplesdir, name = name, execute = true, documenter = true, continue_on_error=true, credit = true)
+if haskey(ENV, "GITHUB_ACTIONS")
+    println("Running inside GitHub Actions CI, skipping execution of notebooks")
+else
+    Literate.notebook(joinpath(examplesdir, file), examplesdir, name = name, execute = false, documenter = true, credit = true)
+    Literate.script(joinpath(examplesdir, file), examplesdir, name = name, keep_comments = false, documenter = true, credit = false)
+    Literate.markdown(joinpath(examplesdir, file), examplesdir, name = name, execute = true, documenter = true, continue_on_error=true, credit = true)
+end
 
